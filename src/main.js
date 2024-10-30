32474c9d4549f2500fc7d10fa658c61a22637db3
GemiALB <131560381+GemiALB@users.noreply.github.com>
2024-10-30 18:05:30 +0100

    ajout de bootstrap projet vue + vite

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -5,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
+
+app.provide('bootstrap', bootstrap);
 
 app.use(createPinia())
 
